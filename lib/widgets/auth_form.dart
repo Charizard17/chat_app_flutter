79e9d4376f6dd7b139075a12be92388c3a670cf8
Esad Dursun <3723esad@gmail.com>
2021-12-25 10:21:28 +0100

    authentication form created

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -6,6 +6,26 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState.save();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+
+      // will be edited for auth requests ...
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,34 +34,76 @@ class _AuthFormState extends State<AuthForm> {
         child: Padding(
           padding: EdgeInsets.all(15),
           child: Form(
+            key: _formKey,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextFormField(
+                  key: ValueKey('email'),
+                  validator: (value) {
+                    if (value.isEmpty || !value.contains('@')) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     labelText: 'Email address',
                   ),
+                  onSaved: (value) {
+                    _userEmail = value;
+                  },
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Username',
+                if (!_isLogin)
+                  TextFormField(
+                    key: ValueKey('username'),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 4) {
+                        return 'Please enter at least 4 characters';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                    ),
+                    onSaved: (value) {
+                      _userName = value;
+                    },
                   ),
-                ),
                 TextFormField(
+                  key: ValueKey('password'),
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 8) {
+                      return 'Password must be at least 8 characters long';
+                    }
+                    return null;
+                  },
                   decoration: InputDecoration(
                     labelText: 'Password',
                   ),
                   obscureText: true,
+                  onSaved: (value) {
+                    _userPassword = value;
+                  },
                 ),
                 SizedBox(height: 10),
                 RaisedButton(
-                  child: Text('Login'),
-                  onPressed: () {},
+                  child: Text(
+                    _isLogin ? 'Login' : 'Signup',
+                  ),
+                  onPressed: _trySubmit,
                 ),
                 TextButton(
-                  child: Text('Create an account'),
-                  onPressed: () {},
+                  child: Text(
+                    _isLogin
+                        ? 'Create an account'
+                        : 'I already have an account',
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isLogin = !_isLogin;
+                    });
+                  },
                 ),
               ],
             ),
