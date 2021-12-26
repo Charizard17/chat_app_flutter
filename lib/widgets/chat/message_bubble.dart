fbeab0fa70050dd54c1369d297464d074de06c55
Esad Dursun <3723esad@gmail.com>
2021-12-26 14:44:17 +0100

    display user image on message bubbles

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -6,58 +6,79 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class MessageBubble extends StatelessWidget {
   final String message;
   final String userName;
+  final String userImage;
   final bool isMe;
   final Key key;
 
-  MessageBubble(this.message, this.userName, this.isMe, {this.key});
+  MessageBubble(this.message, this.userName, this.userImage, this.isMe,
+      {this.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
+      overflow: Overflow.visible,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
-              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
-              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
-            ),
-          ),
-          width: 150,
-          padding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 15,
-          ),
-          margin: EdgeInsets.symmetric(
-            vertical: 5,
-            horizontal: 10,
-          ),
-          child: Column(
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            children: [
-              Text(
-                      userName,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: isMe
-                            ? Colors.black
-                            : Theme.of(context).accentTextTheme.headline1.color,
-                      ),
-                    ),
-              Text(
-                message,
-                style: TextStyle(
-                  color: isMe
-                      ? Colors.black
-                      : Theme.of(context).accentTextTheme.headline1.color,
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+                  bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
                 ),
-                textAlign: isMe ? TextAlign.end : TextAlign.start,
               ),
-            ],
+              width: 150,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 15,
+              ),
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 10,
+              ),
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    userName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: isMe
+                          ? Colors.black
+                          : Theme.of(context).accentTextTheme.headline1.color,
+                    ),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                      color: isMe
+                          ? Colors.black
+                          : Theme.of(context).accentTextTheme.headline1.color,
+                    ),
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: -10,
+          left: isMe ? null : 130,
+          right: isMe ? 130 : null,
+          child: CircleAvatar(
+            backgroundColor: Theme.of(context).primaryColor,
+            radius: 23,
+            child: CircleAvatar(
+              radius: 22,
+              backgroundImage: NetworkImage(userImage),
+            ),
           ),
         ),
       ],
