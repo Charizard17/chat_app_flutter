0eb95ab86cdb314da4e572c240aa565458eb353a
Esad Dursun <3723esad@gmail.com>
2021-12-26 18:07:24 +0100

    firebase_messaging package installed. handle push notifications

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final FirebaseMessaging fcm = FirebaseMessaging.instance;
+    fcm.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification _notification = message.notification;
+      AndroidNotification _android = message.notification?.android;
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     double testVal = 0;
